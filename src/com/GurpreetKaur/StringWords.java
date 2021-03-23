b968da670a7afc32b61fec027308b14e3747f909
Gurpreet Aulakh <gurpreetaulakh2992@gmail.com>
2021-03-23 13:11:28 -0400

    Initial commit

---
+++ src/com/GurpreetKaur/StringWords.java
@@ -0,0 +1,25 @@
+package com.GurpreetKaur;
+
+public class StringWords {
+    public static void main(java.lang.String[] args){
+        java.lang.String str = "hello hello how Are you HELLO are there happy test you";
+        System.out.println("Original String:"+str);
+        java.lang.String[] words=str.split(" ");
+        int count=1;
+        for(int x=0;x<words.length;x++)
+        {
+            for(int y=x+1;y<words.length;y++)
+            {
+                if(words[x].toLowerCase().equals(words[y].toLowerCase()))
+                {
+                    count=count+1;
+                    words[y]="0";
+                }
+            }
+            if(!words[x].equals("0")) {
+                System.out.println(words[x].toLowerCase() + "->" + count);
+            }
+            count=1;
+        }
+    }
+}
